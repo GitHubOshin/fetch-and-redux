c42a071d59fe6d3eb31b6efc877890f04d23ec5b
OSHIN <izkp44@gmail.com>
2023-05-01 04:02:52 +0700

    Create slice

--- src/features/ipAddressDetailsSlice.js
+++ src/features/ipAddressDetailsSlice.js
@@ -1,1 +1,18 @@
 import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  value: {}
+}
+
+export const ipAddressDetailsSlice = createSlice({
+  name: 'ipAddressData',
+  initialState,
+  reducers: {
+    addData: (state, action) => {
+      state.value = action.payload
+    }
+  }
+})
+export const { addData } = ipAddressDetailsSlice.actions
+
+export default ipAddressDetailsSlice.reducer
