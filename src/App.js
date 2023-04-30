b59860c8ef183882a1beacbf731a96791fcda032
OSHIN <izkp44@gmail.com>
2023-05-01 02:16:43 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import './App.css'
 
 function App() {
-  return <div className="App"></div>
+  return (
+    <div className="App">
+      <h1>IP Address Tracker</h1>
+      <br />
+      <table>
+        <tr>
+          <th>IP ADDRESS</th>
+          <th>LOCATION</th>
+          <th>TIMEZONE</th>
+          <th>ISP</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+    </div>
+  )
 }
 
 export default App
